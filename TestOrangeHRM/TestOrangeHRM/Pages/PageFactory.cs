7485a233613757633643c0f07a81b1e6f68c36ff
Harold Dawson III <hdawson@dealeron.com>
2019-03-29 09:21:57 -0400

    Simplified PageFactory to not require updates every time a new page is added, also made thread-safe to allow for parallel testing

--- TestOrangeHRM/TestOrangeHRM/Pages/PageFactory.cs
+++ TestOrangeHRM/TestOrangeHRM/Pages/PageFactory.cs
@@ -1,45 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Runtime.Remoting.Messaging;
 using OpenQA.Selenium;
 
 namespace TestOrangeHRM.Pages
 {
   public static class PageFactory
   {
-    private static readonly Dictionary<Type, BasePage> _internalDictionary = new Dictionary<Type, BasePage>();
+    private static readonly Dictionary<(Type, IWebDriver), BasePage> _internalDictionary = new Dictionary<(Type, IWebDriver), BasePage>();
+    private static readonly object _internalDictionaryLock = new object();
 
     public static T GetPage<T>(IWebDriver driver) where T : BasePage
     {
-      if (_internalDictionary.TryGetValue(typeof(T), out var result))
+      lock (_internalDictionaryLock)
+      {
+        return InternalGetPage<T>(driver);
+      }
+    }
+    private static T InternalGetPage<T>(IWebDriver driver) where T : BasePage
+    {
+      // Use a tuple to make a composite key of type and the driver passed. This allows different drivers to get different instances of the same page type for parallel testing.
+      var key = (typeof(T), driver);
+      if (_internalDictionary.TryGetValue(key, out var result))
       {
         return (T)result;
       }
 
-      BasePage newPage = null;
-
-
-      if (typeof(T) == typeof(HrmSystemUsersPage))
-      {
-        newPage = new HrmSystemUsersPage(driver);
-      }
-      else if (typeof(T) == typeof(HrmPageMenu))
-      {
-        newPage = new HrmPageMenu(driver);
-      }
-      else if (typeof(T) == typeof(HrmLoginPage))
-      {
-        newPage = new HrmLoginPage(driver);
-      }
-
-      if (newPage == null)
-      {
-        throw new InvalidOperationException();
-      }
-
-      _internalDictionary.Add(typeof(T),(T)newPage);
-
-      return (T)newPage;
+      var newPage = (T)Activator.CreateInstance(typeof(T), driver);
+      _internalDictionary.Add(key, newPage);
+      
+      return newPage;
     }
   }
 }
